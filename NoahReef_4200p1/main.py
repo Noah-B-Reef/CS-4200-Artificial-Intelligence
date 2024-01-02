2ad733c05c93f025c08365dd00b21a32f4f66796
Noah-B-Reef <n.reef23@gmail.com>
2024-01-02 14:32:08 -0800

    upadte

--- NoahReef_4200p1/main.py
+++ NoahReef_4200p1/main.py
@@ -120,6 +120,27 @@ def get_neighbors(mat):
 
     return neighbors
 
+class Node:
+    def __init__(self, state, parent, g, h):
+        self.state = state
+        self.parent = parent
+        self.g = g
+        self.h = h
+        self.f = g + h
+
+    def __lt__(self, other):
+        return self.f < other.f
+
+    def __eq__(self, other):
+        if not isinstance(other, Node):
+            return False
+        else:
+            return np.array_equal(self.state, other.state)
+        
+        return np.array_equal(self.state, other.state)
+
+    def __hash__(self):
+        return hash(str(self.state))
 
 def A_star(mat, heuristic):
     
@@ -131,56 +152,71 @@ def A_star(mat, heuristic):
             num_iters - number of iterations
     '''
 
-    # initialize the path, states_visited, and num_iters
+    # intialize frontier and explored
+    frontier = []
+    explored = set()
+
+    # initialize the root node
+    root = Node(mat, None, 0, heuristic(mat))
+
+    # add the root node to the frontier
+    frontier.append(root)
+
+    # initialize the path
     path = []
-    states_visited = []
+
+    # initialize the number of iterations
     num_iters = 0
 
-    # initialize the frontier and explored sets
-    frontier = []
-    explored = []
-
-    # add the initial state to the frontier
-    frontier.append((mat, heuristic(mat)))
-
     # while the frontier is not empty
-    while frontier:
+    while len(frontier) > 0:
+            
+            # sort the frontier
+            frontier.sort()
+    
+            # get the node with the lowest f value
+            node = frontier.pop(0)
+    
+            # check if node is the goal
+            if heuristic(node.state) == 0:
+                # backtrack to get the path
+                while node.parent != None:
+                    path.append(node.state)
+                    node = node.parent
+                path.append(node.state)
+                path.reverse()
+                return path, explored, num_iters
+    
+            # add the node to the explored set
+            explored.add(node)
+    
+            # get the neighbors of the node
+            neighbors = get_neighbors(node.state)
+    
+            # for each neighbor
+            for neighbor in neighbors:
+                # create a node
+                neighbor_node = Node(neighbor, node, node.g + 1, heuristic(neighbor))
+    
+                # check if the neighbor is in the frontier or explored
+                if neighbor_node not in frontier and neighbor_node not in explored:
+                    # add the neighbor to the frontier
+                    frontier.append(neighbor_node)
+                    num_iters += 1
+    
+                # check if the neighbor is in the frontier
+                elif neighbor_node in frontier:
+                    # get the node in the frontier
+                    frontier_node = frontier[frontier.index(neighbor_node)]
+    
+                    # check if the neighbor has a lower f value
+                    if neighbor_node.f < frontier_node.f:
+                        # replace the frontier node with the neighbor node
+                        frontier[frontier.index(neighbor_node)] = neighbor_node
+                        num_iters += 1
 
-        # sort the frontier by the heuristic
-        frontier.sort(key = lambda x: x[1])
-
-        # pop the state with the lowest heuristic from the frontier
-        state = frontier.pop(0)
-
-        # add the state to the explored set
-        explored.append(state[0])
-
-        # add the state to the path
-        path.append(state[0])
-
-        # add the state to the states_visited
-        states_visited.append(state[0])
-
-        # if the state is the goal state
-        if np.array_equal(state[0], np.matrix([[0,1,2],[3,4,5],[6,7,8]])):
-            return path, states_visited, num_iters
-
-        # get the neighbors of the state
-        neighbors = get_neighbors(state[0])
-
-        # for each neighbor
-        for neighbor in neighbors:
-
-            # if the neighbor is not in the frontier or explored set
-            if  not any(np.array_equal(neighbor, state) for state in frontier) and not any(np.array_equal(neighbor, state) for state in explored):
-
-                # add the neighbor to the frontier
-                frontier.append((neighbor, heuristic(neighbor)))
-
-        # increment the number of iterations
-        num_iters += 1
-
-    return path, states_visited, num_iters
+                
+  
 
 def read_file(filename):
     '''
@@ -208,26 +244,159 @@ def read_file(filename):
     
     return puzzles
 
+def generate_puzzles(num, depth = 100):
+    '''
+    Summary: Generates a list of 8-puzzles
+    Input: depth - depth of 8-puzzles to generate
+    Output: list of 8-puzzles
+    '''
+
+    # initialize the list of 8-puzzles
+    puzzles = []
+
+    # generate the 8-puzzles
+    for i in range(num):
+        puzzle = np.matrix([[0,1,2],[3,4,5],[6,7,8]])
+        for j in range(int(depth)):
+            neighbors = get_neighbors(puzzle)
+            puzzle = neighbors[np.random.randint(0,len(neighbors))]
+        puzzles.append(puzzle)
+
+    return puzzles
+
 def Experiment():
+
+    '''
+    Summary: Performs the experiment for the 8-puzzle. Saves results to csv files
+    Input: None
+    Output: None
+    '''
+
+    avg_search_mahattan = []
+    avg_time_mahattan = []
+
+    avg_search_displaced = []
+    avg_time_displaced = []
+
     for i in range(4,21,4):
-        df = pd.DataFrame(columns = ["Length", "Manhattan Distance", "Manhattan Distance Time", "Displaced Tiles", "Displaced Tiles Time"])
+
+        # create csv file
+        df = pd.DataFrame(columns = ["d", "Manhattan Distance", "Manhattan Distance Time", "Displaced Tiles", "Displaced Tiles Time"])
+
+        # averages the results for each length
+        mahattan_searches = []
+        displaced_searches = []
+
+        mahattan_times = []
+        displaced_times = []
+
+
         puzzles = read_file("Length" + str(i) + ".txt")
         for puzzle in puzzles:
             if check_is_valid(puzzle):
                 start = time.time()
-                path_mahat, states_visited, num_iters = A_star(puzzle, manhattan_distances)
+                path_mahat, states_visited_mahat, num_iters = A_star(puzzle, manhattan_distances)
                 end = time.time()
                 manhattan_time = end - start
 
                 start = time.time()
-                path_displaced, states_visited, num_iters = A_star(puzzle, displaced_distances)
+                path_displaced, states_visited_displaced, num_iters = A_star(puzzle, displaced_distances)
                 end = time.time()
                 displaced_time = end - start
 
-                df = pd.concat([df, pd.DataFrame([[len(path_mahat), manhattan_distances(puzzle), manhattan_time, displaced_distances(puzzle), displaced_time]], columns = ["Length", "Manhattan Distance", "Manhattan Distance Time", "Displaced Tiles", "Displaced Tiles Time"])])        
+                mahattan_searches.append(len(states_visited_mahat))
+                displaced_searches.append(len(states_visited_displaced))
+
+                mahattan_times.append(manhattan_time)
+                displaced_times.append(displaced_time)
+
+                df = pd.concat([df, pd.DataFrame([[i,len(states_visited_mahat), manhattan_time, len(states_visited_mahat), displaced_time]], columns = ["d", "Manhattan Distance", "Manhattan Distance Time", "Displaced Tiles", "Displaced Tiles Time"])])        
                 df.to_csv("Length" + str(i) + ".csv", index = False)
+        
+        avg_search_mahattan.append(np.mean(mahattan_searches))
+        avg_time_mahattan.append(np.mean(mahattan_times))
+
+        avg_search_displaced.append(np.mean(displaced_searches))
+        avg_time_displaced.append(np.mean(displaced_times))
+
+    df = pd.DataFrame(columns = ["Length", "Avg Manhattan Distance", " Avg Manhattan Distance Time", "Avg Displaced Tiles", "Avg Displaced Tiles Time"])
+    df = pd.concat([df, pd.DataFrame([[4, avg_search_mahattan[0], avg_time_mahattan[0], avg_search_displaced[0], avg_time_displaced[0]]], columns = ["Length", "Avg Manhattan Distance", " Avg Manhattan Distance Time", "Avg Displaced Tiles", "Avg Displaced Tiles Time"])])
+    df = pd.concat([df, pd.DataFrame([[8, avg_search_mahattan[1], avg_time_mahattan[1], avg_search_displaced[1], avg_time_displaced[1]]], columns = ["Length", "Avg Manhattan Distance", " Avg Manhattan Distance Time", "Avg Displaced Tiles", "Avg Displaced Tiles Time"])])
+    df = pd.concat([df, pd.DataFrame([[12, avg_search_mahattan[2], avg_time_mahattan[2], avg_search_displaced[2], avg_time_displaced[2]]], columns = ["Length", "Avg Manhattan Distance", " Avg Manhattan Distance Time", "Avg Displaced Tiles", "Avg Displaced Tiles Time"])])
+    df = pd.concat([df, pd.DataFrame([[16, avg_search_mahattan[3], avg_time_mahattan[3], avg_search_displaced[3], avg_time_displaced[3]]], columns = ["Length", "Avg Manhattan Distance", " Avg Manhattan Distance Time", "Avg Displaced Tiles", "Avg Displaced Tiles Time"])])
+    df = pd.concat([df, pd.DataFrame([[20, avg_search_mahattan[4], avg_time_mahattan[4], avg_search_displaced[4], avg_time_displaced[4]]], columns = ["Length", "Avg Manhattan Distance", " Avg Manhattan Distance Time", "Avg Displaced Tiles", "Avg Displaced Tiles Time"])])
+    df.to_csv("Summary.csv", index = False)
 
 
 def main():
-   Experiment()
+    print("CS 4200 Project 1")
+
+    # Prompt user for input
+    print("Select:")
+    print("1. Single Test Puzzle")
+    print("2. Exit")
+
+    print()
+    choice = input("Enter Choice: ")
+    print()
+
+    if choice == '3':
+        exit()
+
+    num_of_puzzles = 1
+
+    print("Select Input Method:")
+    print("1. Manual")
+    print("2. Random")
+    print()
+    input_method = input("Enter Choice: ")
+    print()
+
+    if input_method == '1':
+        row1 = input("Enter Row 1: ")
+        row2 = input("Enter Row 2: ")
+        row3 = input("Enter Row 3: ")
+        
+    
+    print("Select Heuristic:")
+    print("1. Manhattan Distance")
+    print("2. Displaced Tiles")
+    print()
+    heuristic = input("Enter Choice: ")
+    print()
+
+    if input_method == '1':
+        puzzle = np.matrix([[int(num) for num in row1.split(" ")] ,[int(num) for num in row2.split(" ")] ,[int(num) for num in row3.split(" ")]])
+    else:
+        puzzle = generate_puzzles(num_of_puzzles)[0]
+
+    print("Puzzle:")
+    print(puzzle)
+
+    
+    if heuristic == '1':
+        start = time.time()
+        path, states_visited, num_iters = A_star(puzzle, manhattan_distances)
+        end = time.time()
+    else:
+        start = time.time()
+        path, states_visited, num_iters = A_star(puzzle, displaced_distances)
+        end = time.time()
+
+    print("Path:")
+    count = 1
+    for state in path:
+        print("Step " + str(count))
+        count += 1
+
+        # pretty print state
+        print(state[0,0], state[0,1], state[0,2])
+        print(state[1,0], state[1,1], state[1,2])
+        print(state[2,0], state[2,1], state[2,2])
+        print()
+        print()
+
+    print("Search Cost: " + str(len(states_visited)))
+    print("Time: " + str(end - start))
+
 main()
